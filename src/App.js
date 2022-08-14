94e13eff97613d205a6886688a809e924f2f80bf
ikramulSoHeL <93004833+ikramulSoHeL@users.noreply.github.com>
2022-08-14 21:59:46 +0600

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Slidebar from './components/Slidebar/Slidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Slidebar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Slidebar/Slidebar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Slidebar.css'
+
+const Slidebar = () => {
+  return (
+    <div>Slidebar</div>
+  )
+}
+
+export default Slidebar
