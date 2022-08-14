aefe8b81758528768dfdd69b0fa1e193bc41d2f8
ikramulSoHeL <93004833+ikramulSoHeL@users.noreply.github.com>
2022-08-14 22:38:51 +0600

    sidebar

--- src/components/Slidebar/Slidebar.jsx
+++ src/components/Slidebar/Slidebar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Slidebar.css'
 
+import Logo from '../../img/logo.png'
+
 const Slidebar = () => {
   return (
-    <div>Slidebar</div>
+    <div className='Sidebar'>
+        {/* logo */}
+        <div className='logo'>
+            <img src={Logo} alt="" />
+            <span>
+                Sh<span>o</span>ps
+            </span>
+        </div>
+    </div>
   )
 }
 
