97270b01988ef14c80b06d16e3a2cb620374c881
ikramulSoHeL <93004833+ikramulSoHeL@users.noreply.github.com>
2022-08-15 03:23:16 +0600

    Sidebar Updated

--- src/components/Slidebar/Slidebar.jsx
+++ src/components/Slidebar/Slidebar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Slidebar.css'
 
 import Logo from '../../img/logo.png'
+import { SidebarData } from '../../Data/Data'
+import { UilSignOutAlt } from '@iconscout/react-unicons'
 
 const Slidebar = () => {
+
+  const [selected, setSelected] = useState(0)
+
+
   return (
     <div className='Sidebar'>
         {/* logo */}
@@ -12,6 +18,28 @@ const Slidebar = () => {
             <span>
                 Sh<span>o</span>ps
             </span>
+        </div>
+
+        {/* menu */}
+        <div className='menu'>
+          {SidebarData.map((item, index)=>{
+            return(
+              <div className={selected===index?'menuItem active': 'menuItem'}
+              key={index}
+              onClick={()=>setSelected(index)}
+              >
+                <item.icon />
+                <span>
+                  {item.heading}
+                </span>
+              </div>
+            )
+          })}
+
+          <div className='menuItem'>
+            <UilSignOutAlt />
+          </div>
+
         </div>
     </div>
   )
